bfb84a2bf2ab066f008cfc69a69b66a420aa6311
smoliver <samuelolive21@gmail.com>
2017-01-19 14:42:44 -0500

    Multiplyers formatted

--- source/index.js
+++ source/index.js
@@ -1,25 +1,69 @@
 // Literals: ',' '/'
-// 
+// Value combinators ' [ ] && | || '
+// Value multiplyers '* + ? {int,int} # !'
 
 let breakPoints = /\s,\//
 
 class CSSValdef {
 
-// Ensures propper spacing between value combinators, literals, and variables
-// Value combinators' <.+> [ ] , / | || '
-
+// Ensures propper spacing between value combinators, value multiplyers, literals, and variables
   static format (stringDefinition = '') {
-    return stringDefinition.replace(/(\|\||\||&&|\[|\]|,|\/|<[^>]*>)/g, ' $1 ').replace(/\s+/g, ' ').trim();
+    return stringDefinition.replace(/(\|\||\||&&|\[|\]|,(?=[^{}]*(?:{|$))|\/|<[^>]*>)/g, ' $1 ')
+      .replace(/\s*(\*|\+|\?|\{[^\}]*\}|\#|\!)/g, '$1 ').replace(/\s+/g, ' ').trim();
   }
+
 // Checks propper spacing between value combinators, literals, and variables
-// Checks for valid variable syntax
-// Throws an error in that order if not true
+// Throws an error if there is an error
   static validate (stringDefinition = '') {
     if(stringDefinition != CSSValdef.format (stringDefinition)){
       throw new Error(`Impropper CSS Value Definition syntax:\
         \n\tExpected: ${CSSValdef.format (stringDefinition)}\
-        \n\tActual: ${stringDefinition}`);
+        \n\tActual: ${stringDefinition}`
+      );
     }
+  }
+
+  // Checks for valid variable names
+  // Throws an error otherwise 
+  static validateVariables (stringDefinition = '') {
+    let badVariabledef = /(?:<(?:""|'')?>)|(?:<'[a-z\-]*[^a-z\->]+[^'>]*'>)|(?:<"[a-z\-]*[^a-z\->]+[^">]*">)|(?:<[a-z\-]*[^a-z\->]+[^>]*>)/g
+    let badVariables = stringDefinition.match(badVariabledef);
+
+    if(badVariables && badVariables.length){
+      throw new Error (`Invalid Variables:\
+        \n\t${badVariables.join (', ')}
+        \n\tVariable names may only include lowercase letters and '-'`);
+    }
+  }
+
+  // Ensures that all braces, parentheses, and box backets have a full pair
+  // Throws an error, including the responsible bracket and the count
+  static validateBrackets (stringDefinition = ''){
+    let leftBraceCount = (stringDefinition.match ('{') || []).length;
+    let rightBraceCount = (stringDefinition.match ('}') || []).length;
+    let leftParenCount = (stringDefinition.match ('(') || []).length;
+    let rightParenCount = (stringDefinition.match (')') || []).length;
+    let leftBoxBracketCount = (stringDefinition.match ('[') || []).length;
+    let rightBoxBracketCount = (stringDefinition.match (']') || []).length;
+
+    // If there are any braces, parentheses, or box brackets without pair, throw an error
+    if (leftBraceCount != rightBraceCount ||
+        leftParenCount != rightParenCount ||
+        leftBoxBracketCount != rightBoxBracketCount) {
+
+      let errorMessage = 'Not all brackets are closed';
+      if (leftBraceCount != rightBraceCount) {
+        errorMessage += `\n\tThere are ${leftBraceCount} '{' and ${rightBraceCount} '}'`;
+      }
+      if (leftParenCount != rightParenCount) {
+        errorMessage += `\n\tThere are ${leftParenCount} '(' and ${rightParenCount} ')'`;
+      }
+      if (leftBoxBracketCount != rightBoxBracketCount) {
+        errorMessage += `\n\tThere are ${leftBoxBracketCount} '[' and ${leftBoxBracketCount} ']'`;
+      }
+      throw new Error (errorMessage);
+    }
+
   }
 
   constructor (valueDefinition = '') {
--- test/test.js
+++ test/test.js
@@ -2,28 +2,22 @@ import { CSSValdef } from '../distribution/index.js';
 import assert from 'assert';
 
 describe ('CSSValdef', function () {
-  describe ('test', function () {
-    let value = 'One value';
-    let otherValue = 'Another value';
-    let valueDef = new CSSValdef (value);
-    
-    it ('Should return true when the values are equal', function () {
-      assert.ok (valueDef.test (value));
-    });
-    it ('Should return false when the values are not euqal', function (){
-      assert.ok (!valueDef.test (otherValue))
-    });
-  });
+  // Tests the format function 
   describe ('format', function () {
     it ('Should place one space between value combinators, literals, and variables', function () {
-      let squishedSyntax = '[[left||right]&&<length>|<width>]|inherit';
-      let propperSyntax = '[ [ left || right ] && <length> | <width> ] | inherit';
+      let squishedSyntax = '[[left||right]&&<length>|<width>] |inherit,game';
+      let propperSyntax = '[ [ left || right ] && <length> | <width> ] | inherit , game';
       assert.equal (CSSValdef.format(squishedSyntax), propperSyntax);
     });
-    it ('Should replace all white space characters with a space, and remove extras', function () {
+    it ('Should remove all extra white space and replace white space characters with a single space', function () {
       let spacySyntax = '[  [    left \t|| right \f]\n&&\r<length>\t| <width>\v] | inherit'
       let propperSyntax = '[ [ left || right ] && <length> | <width> ] | inherit';
       assert.equal (CSSValdef.format(spacySyntax), propperSyntax);
+    });
+    it ('should ensure no space before, and a single space after all multiplyers', function () {
+      let spacyMultiplyers = 'one    ?two {10,14}three!four# five+ six *';
+      let propperMultiplyer = 'one? two{10,14} three! four# five+ six*';
+      assert.equal (CSSValdef.format(spacyMultiplyers), propperMultiplyer);
     });
     it ('Should remove leading and trailing spaces', function () {
       let misleadingSyntax = '   [ [ left || right ] && <length> | <width> ] | inherit   ';
@@ -31,11 +25,27 @@ describe ('CSSValdef', function () {
       assert.equal (CSSValdef.format(misleadingSyntax), propperSyntax);
     });
   });
+  // Testing for the Validate Layout Function
   describe ('validate', function (){
+    // Test Propper spacing
     it ('Should throw an impropper CSS Value Definition syntax error', function () {
       let squishedSyntax = '[ [ left|| right ] && <length> | <width> ] | inherit'
-      let syntaxError = /Impropper CSS Value Definition syntax/;
-      assert.throws(CSSValdef.validate.bind(this, squishedSyntax), syntaxError)
+      let caughtErrorMessage = /Impropper CSS Value Definition syntax/;
+      assert.throws (CSSValdef.validate.bind (this, squishedSyntax), caughtErrorMessage, 
+        `\n     Impropper syntax not raising error:\n\t${squishedSyntax}`);
     });
+    it ('Should not throw an error', function () {
+      let propperSyntax = '[ [ left || right ] && <length> | <width> ] | inherit';
+      assert.doesNotThrow (CSSValdef.validate.bind (this, propperSyntax), 
+        `\n     Propper syntax raising unexpected error\n\t${propperSyntax}`)
+    });
+  })
+  // Testing for the Validate Variables Function
+  describe ('Validate Variable Names', function (){
+    let errorDescription = `\n     Impropper variable name not raising error:`
+    let thrownError = /Invalid Variables/;
+    it ('Should throw an error for empty variable names', function () {
+      assert.throws (CSSValdef.validateVariables.bind (this, '<>'), thrownError, errorDescription)
+    })
   })
 });
